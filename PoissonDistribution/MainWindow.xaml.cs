34ef1f9ac46adb7558eefdc51e1a582a45a72981
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-11-15 23:51:50 +0300

    Added new functionality to the calculator

--- PoissonDistribution/MainWindow.xaml.cs
+++ PoissonDistribution/MainWindow.xaml.cs
@@ -25,6 +25,8 @@ namespace PoissonDistribution
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private Poisson poissonDistribution;
+        private double[] probabilities;
+        private string[] mCollection;
         public SeriesCollection SeriesCollection { get; set; }
         public string[] XFormatter { get; set; }
         public Func<double, string> YFormatter { get; set; }
@@ -44,8 +46,11 @@ namespace PoissonDistribution
             try
             {
                 poissonDistribution = new Poisson(int.Parse(txtCountTests.Text),
-                    double.Parse(txtProbability.Text), int.Parse(txtM.Text));
-                printChart(poissonDistribution);
+                    double.Parse(txtProbability.Text));
+                probabilities = poissonDistribution.getProbabilities();
+                mCollection = poissonDistribution.getMCollection();
+                printChart();
+                printText();
                 DataContext = this;
             }
             catch(Exception ex)
@@ -53,24 +58,215 @@ namespace PoissonDistribution
                 MessageBox.Show(ex.Message);
             }
         }
-        private void printChart(Poisson pd)
+        private void printChart()
         {
-            double[] propabilities = pd.getProbabilities();
-
             SeriesCollection = new SeriesCollection
             {
                 new LineSeries
                 {
                     Title = string.Empty,
-                    Values = new ChartValues<double> (propabilities)                    
+                    Values = new ChartValues<double> (probabilities)                    
                 },             
             };
-            XFormatter = pd.getMCollection();
+            XFormatter = mCollection;
             YFormatter = value => value.ToString("F6");
 
             OnPropertyChanged(nameof(SeriesCollection));
             OnPropertyChanged(nameof(XFormatter));
             OnPropertyChanged(nameof(YFormatter));
+        }
+
+
+        private void printText()
+        {
+            spTextcontent.Children.Clear();
+            spTextcontent.Children.Add(new Label
+            {
+                Content = $"Математическое ожидание M[X] = λ = n * p = {poissonDistribution.Disperion}"
+            });
+            spTextcontent.Children.Add(new Label
+            {
+                Content = $"Дисперсия D[X] = λ = n * p = {poissonDistribution.Disperion}"
+            });
+            spTextcontent.Children.Add(new Label
+            {
+                Content = $"Найдём ряд распределения :"
+            });
+            for(int i = 0; i < probabilities.Length; i++)
+            {
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = $"P({i}) = {poissonDistribution.Disperion}^{i} * e^{poissonDistribution.Disperion} / {i}! = {probabilities[i]}"
+                }) ;
+            }
+            
+            if (cbWillCome.IsChecked == true 
+                && int.TryParse(txtWillCome.Text, out int resultWillCome))
+            {
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = $"Вероятность того, что событие наступит ровно {resultWillCome} раза"
+                });
+                string strWillCome = $"P({resultWillCome}) = {poissonDistribution.Disperion}^{resultWillCome} " +
+                    $"* e^{poissonDistribution.Disperion} / {resultWillCome}!";
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = resultWillCome < probabilities.Count() ? strWillCome + $" = {probabilities[resultWillCome]}" 
+                        : strWillCome + " = 0",
+                    FontWeight = FontWeights.Bold
+                });
+            }
+
+            if(cbLess.IsChecked == true
+                && int.TryParse(txtLess.Text, out int resultLess))
+            {
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = $"Вероятность того, что событие наступит менее {resultLess} раз."
+                });
+                int index = 0;
+                double sumLess = 0.0;
+                string strLess = $"P(X < {resultLess}) = " ;
+                while(index < resultLess)
+                {
+                    if (probabilities.Length >= index + 1)
+                    {
+                        sumLess += probabilities[index];
+                        if(index != 0)
+                            strLess += $" + {probabilities[index].ToString("F5")}";
+                        else
+                            strLess += $"{probabilities[index].ToString("F5")}";
+                    }
+                    index++; 
+                }
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = strLess + $" = {Math.Round(sumLess,5)}",
+                    FontWeight = FontWeights.Bold
+                });
+            }
+
+            if(cbNotLess.IsChecked == true
+                && int.TryParse(txtNotLess.Text, out int resultNotLess))
+            {
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = $"Вероятность того, что событие наступит не менее {resultNotLess} раз."
+                });
+                int index = 0;
+                double sumNotLess = 0.0;
+                string strNotLess = $"P(X >= {resultNotLess}) = 1 - (";
+                while (index < resultNotLess)
+                {
+                    if (probabilities.Length >= index + 1)
+                    {
+                        sumNotLess += probabilities[index];
+                        strNotLess += $" + {probabilities[index].ToString("F5")}";
+                    }
+                    index++;
+                }
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = strNotLess + $") = {Math.Round(1 - sumNotLess, 5)}",
+                    FontWeight = FontWeights.Bold
+                });
+            }
+
+            if(cbMore.IsChecked == true
+                && int.TryParse(txtMore.Text, out int resultMore))
+            {
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = $"Вероятность того, что событие наступит более {resultMore} раз."
+                });
+                double sumMore = 0.0;
+                string strMore = $"P(x > {resultMore}) = ";
+                for(int i = resultMore + 1; i < probabilities.Length; i++)
+                {
+                    sumMore += probabilities[i];
+                    if (i == resultMore + 1)
+                        strMore += $"{probabilities[i]}";
+                    else
+                        strMore += $" + {probabilities[i]}";
+                }
+                spTextcontent.Children.Add(new TextBlock
+                {
+                    Text = strMore + $" = {Math.Round(sumMore, 5)}",
+                    FontWeight = FontWeights.Bold,
+                    TextWrapping = TextWrapping.WrapWithOverflow
+                });
+            }
+
+            if(cbNoMore.IsChecked == true
+                && int.TryParse(txtNoMore.Text, out int resultNoMore))
+            {
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = $"Вероятность того, что событие наступит не более {resultNoMore} раз."
+                });
+                double sumNoMore = 0.0;
+                string strNoMore = $"P(X <= {resultNoMore}) = ";
+                for (int i = resultNoMore + 1; i < probabilities.Length; i++)
+                {
+                    sumNoMore += probabilities[i];
+                    if (i == resultNoMore + 1)
+                        strNoMore += $"1 - ({probabilities[i]}";
+                    else
+                        strNoMore += $" + {probabilities[i]}";
+                }
+                spTextcontent.Children.Add(new TextBlock
+                {
+                    Text = strNoMore + $") = {1.0 - Math.Round(sumNoMore, 5)}",
+                    FontWeight = FontWeights.Bold,
+                    TextWrapping = TextWrapping.WrapWithOverflow
+                });
+            }
+
+            if(cbNotLessNoMore.IsChecked == true
+                && int.TryParse(txtNotLessNotMore1.Text, out int resultNotLessNoMore1)
+                && int.TryParse(txtNotLessNotMore2.Text, out int resultNotLessNoMore2))
+            {
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = $"Вероятность того, что событие наступит не менее {resultNotLessNoMore1} " +
+                    $"и не более {resultNotLessNoMore2} раз."
+                });
+                double sumNotLessNoMore = 0.0;
+                string strNotLessNoMore = $"P({resultNotLessNoMore1} <= X <= {resultNotLessNoMore2}) = ";
+                for(int i = resultNotLessNoMore1; i < resultNotLessNoMore2 + 1; i++)
+                {
+                    if (probabilities.Length == i)
+                        break;
+                    sumNotLessNoMore += probabilities[i];
+                    if (i == resultNotLessNoMore1)
+                        strNotLessNoMore += $"{probabilities[i]}";
+                    else
+                        strNotLessNoMore += $" + {probabilities[i]}";
+                }
+                spTextcontent.Children.Add(new TextBlock
+                {
+                    Text = strNotLessNoMore + $" = {Math.Round(sumNotLessNoMore, 5)}",
+                    FontWeight = FontWeights.Bold,
+                    TextWrapping = TextWrapping.WrapWithOverflow
+                });
+            }
+
+            if(cbStepAtLeastOnce.IsChecked == true)
+            {
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = $"Вероятность того, что событие наступит хотя бы раз"
+                });
+                string strStepAtLeastOnce = $"P(1) = {poissonDistribution.Disperion}^1 " +
+                    $"* e^{poissonDistribution.Disperion} / 1!";
+                spTextcontent.Children.Add(new Label
+                {
+                    Content = 1 < probabilities.Count() ? strStepAtLeastOnce + $" = {probabilities[1]}"
+                        : strStepAtLeastOnce + " = 0",
+                    FontWeight = FontWeights.Bold
+                });
+            }
+            
         }
 
         private void btnCloseWindow_Click(object sender, RoutedEventArgs e) 
--- PoissonDistribution/Poisson.cs
+++ PoissonDistribution/Poisson.cs
@@ -8,11 +8,12 @@ namespace PoissonDistribution
 {
     public class Poisson
     {
-        public Poisson(int countTests, double probability, int m)
+        private List<double> probabilities;
+        public Poisson(int countTests, double probability)
         {
             CountTests = countTests;
             Probability = probability;
-            M = m;
+           
             //double[] factmas = new double[20];
             //for (int i = 0; i < factmas.Length; i++)
             //{
@@ -21,24 +22,34 @@ namespace PoissonDistribution
         }
         public int CountTests { get; }
         public double Probability { get; }
-        public int M { get; }
         public double Disperion => CountTests * Probability;
 
         public double[] getProbabilities()
         {
-            double[] probalities = new double[M];
-            for(int i = 0; i < M; i++)
+            //double[] probalities = new double[M];
+            //for(int i = 0; i < M; i++)
+            //{
+            //    probalities[i] = Math.Pow(Disperion, i + 1) * Math.Exp(-1.0 * Disperion) / factorial(i + 1);
+            //}
+            //return probalities;
+            probabilities = new List<double>();
+            int countM = 0;
+            double probMax = 1.0;
+            while(probMax > 0.000009)
             {
-                probalities[i] = Math.Pow(Disperion, i + 1) * Math.Exp(-1.0 * Disperion) / factorial(i + 1);
+                double prob = Math.Pow(Disperion, countM) * Math.Exp(-1.0 * Disperion) / factorial(countM);
+                probabilities.Add(prob);
+                countM++;
+                probMax -= prob;
             }
-            return probalities;
+            return probabilities.ToArray();
         }
         public string[] getMCollection()
         {
-            string[] mStrings = new string[M];
-            for(int i = 1; i <= M; i++)
+            string[] mStrings = new string[probabilities.Count()];
+            for(int i = 0; i < probabilities.Count(); i++)
             {
-                mStrings[i - 1] = i.ToString();
+                mStrings[i] = i.ToString();
             }
             return mStrings;
         }
